964097300a549f2530cbed07cc665b56cffcf4a0
MarcoBucceri98 <146425393+MarcoBucceri98@users.noreply.github.com>
2023-10-18 17:32:48 +0200

    Add files via upload

---
+++ src/Veicolo.java
@@ -0,0 +1,62 @@
+import java.util.Random;
+
+public class Veicolo {
+    private int id;
+    private String posizione;
+    private boolean affittato;
+    private static int count =0;
+
+    public Veicolo(){
+        this.id = count;
+        count++;
+    }
+
+    public Veicolo(int id, String posizione, boolean affittato) {
+        this.id = id;
+        this.posizione = posizione;
+        this.affittato = false;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPosizione() {
+        return posizione;
+    }
+
+    public void setPosizione(String posizione) {
+        this.posizione = posizione;
+    }
+
+    public boolean isAffittato() {
+        return affittato;
+    }
+
+    public void setAffittato(boolean affittato) {
+        this.affittato = affittato;
+    }
+
+    //il veicolo è stato noleggiato da un utente
+    public void affitoVeicolo() {
+        affittato = true;
+    }
+
+    //il veicolo è stato restituito ed è ora disponibile per il noleggio
+    public void returnVeicolo() {
+        affittato = false;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Veicolo"
+                + "id="
+                + id
+                + ", posizione='"
+                + posizione
+                + ", affittato="
+                + affittato;
+    }
+}
+
