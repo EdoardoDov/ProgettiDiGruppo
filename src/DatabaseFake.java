012378fa7dc83eb86d053583eb4e3142c95f74bd
Edoardo <edoardo2.dov@outlook.it>
2023-10-19 20:15:20 +0200

    aggiornato il db versione 1.0.4

--- src/DatabaseFake.java
+++ src/DatabaseFake.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class DatabaseFake {
     List<Veicolo> flotta;
@@ -14,8 +11,8 @@ public class DatabaseFake {
         this.veicoliNoleggiati = new ArrayList<>();
         this.utenti = new HashMap<>();
     }
-
-    public void aggiungi(Veicolo v){
+    //Metodi relativi ai veicoli
+    public void aggiungiVeicolo(Veicolo v){
         flotta.add(v);
         veicoliDisponibili.add(v);
     }
@@ -36,10 +33,62 @@ public class DatabaseFake {
     public List<Veicolo> getListaVeicoliDisponibili(){
         return veicoliDisponibili;
     }
+    //ricerca il veicolo secondo il tag di ricerca (tipo di veicolo);
+    public Veicolo searchVeicolo(){
+        List<Veicolo> listraFiltrata = new ArrayList<>();
+        int tmp;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("- inserisci 1 per automobile \n- inserisci 2 per furgoncini \n- inserisci 3 per scooter" +
+                            "\n- inserisci 4 per monopattino \n- inserisci 5 per bicicletta");
+        tmp = sc.nextInt();
+        if(tmp ==1 ) {
+            for (Veicolo v : veicoliDisponibili) {
+                if (v instanceof Automobile) {
+                    listraFiltrata.add(v);
+                }
+            }
+        } else if (tmp == 2 ) {
+            for (Veicolo v : veicoliDisponibili) {
+                if (v instanceof Furgoncino) {
+                    listraFiltrata.add(v);
+                }
+            }
+        } else if (tmp == 3) {
+            for (Veicolo v : veicoliDisponibili) {
+                if (v instanceof Scooter) {
+                    listraFiltrata.add(v);
+                }
+            }
+        }else if (tmp == 4) {
+            for (Veicolo v : veicoliDisponibili) {
+                if (v instanceof Monopattino) {
+                    listraFiltrata.add(v);
+                }
+            }
+        }else if (tmp == 5) {
+            for (Veicolo v : veicoliDisponibili) {
+                if (v instanceof Bicicletta) {
+                    listraFiltrata.add(v);
+                }
+            }
+        } else {
+            System.out.println("Input errato!");
+        }
+        System.out.println(listraFiltrata);
+        return selezionaMezzo(listraFiltrata,sc);
+        //dopo che ha visualizzato la lista dei veicoli desiderati chiama il metodo seleziona mezzo che ritorna il veicolo desiderato
+    }
+
+    public Veicolo selezionaMezzo(List<Veicolo> listaFiltrata, Scanner s){
+        int index;
+        System.out.println("inserisci un indice da 0 a n-1");
+        index = s.nextInt();
+        return listaFiltrata.get(index);
+    } //implementare try catch per evitare di andare fuori indice
+
 
     public List<Utente> getUtenti() {
-
-        return null;
+        return new ArrayList<>(utenti.values());
     }
 
     public Utente getUtente(){
@@ -52,7 +101,7 @@ public class DatabaseFake {
                 "flotta=" + flotta + "\n"+
                 ", veicoliNoleggiati=" + veicoliNoleggiati + "\n" +
                 ", veicoliDisponibili=" + veicoliDisponibili + "\n" +
-                ", utenti= "+"\n" + utenti + "\n" +
+                ", utenti= " + utenti + "\n" +
                 '}';
     }
 }
