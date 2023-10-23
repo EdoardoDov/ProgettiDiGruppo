2b6304904598f2906cbad22fd4900a2200336e90
MarcoBucceri98 <146425393+MarcoBucceri98@users.noreply.github.com>
2023-10-23 09:02:33 +0200

    Update Testing.java

--- src/Testing.java
+++ src/Testing.java
@@ -4,13 +4,29 @@ import java.util.Scanner;
 
 public class Testing {
 
-   
-    Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) { 
-        Utente u = new Utente;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        Utente u = new Utente("Davide", "Kappa", "KPPDVD", "21/03/95", false, 'B');
+        Utente u2 = new Utente("Dito", "Mano", "DMTP", "21/03/95", true, 'A');
+        Utente u3 = new Utente();
         SharedMobility sharedMobility = new SharedMobility();
         sharedMobility.registraUtente(u);
+        sharedMobility.affittaVeicolo(u);
+        sharedMobility.registraUtente(u2);
+        sharedMobility.registraUtente(u3);
+        sharedMobility.affittaVeicolo(u2);
+        System.out.println("----------------------");
+        System.out.println(u.toString());
+        System.out.println("----------------------");
+        sharedMobility.restituisciVeicolo(u);
+        System.out.println("----------------------");
+        System.out.println(u.toString());
+
+
 
     }
 
